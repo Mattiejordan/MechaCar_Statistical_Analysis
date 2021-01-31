b0c38ee7e840e55ae5736ad51ca66fbb0beb7c23
Mattiejordan <jordan.mattiem@gmail.com>
2021-01-31 14:04:17 -0600

    deliv 4

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,9 +6,11 @@ library(readr)
 #deliverable 1
 MechaCar_mpg <- read.csv("MechaCar_mpg.csv")
 lm(MechaCar_mpg)
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=MechaCar_mpg)
-carlm <- lm(MechaCar_mpg)
-summary(carlm)
+#lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=MechaCar_mpg)
+mpg_model <- lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length, data=MechaCar_mpg)
+#carlm <- lm(MechaCar_mpg)
+#summary(carlm)
+summary(mpg_model)
 
 #deliverable 2
 Suspension_Coil <- read.csv("Suspension_Coil.csv")
@@ -26,7 +28,8 @@ SuspensionManLotsum <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summa
 #t.test(log10(sample_table$Miles_Driven),mu=mean(log10(population_table$Miles_Driven))) #compare sample versus population means
 #shapiro.test(mtcars$wt)
 
-t.test(Suspension_Coil$PSI,mu=as.numeric(SuspensionPSIsum[1:1])) #compare sample versus population means
+#t.test(Suspension_Coil$PSI,mu=as.numeric(SuspensionPSIsum[1:1])) #compare sample versus population means
+t.test(Suspension_Coil$PSI,mu=1500)
 
 #Lot1
 #t.test(subset(Suspension_Coil, Manufacturing_Lot = "Lot1")$PSI,mu=as.numeric(SuspensionPSIsum[1:1]))
